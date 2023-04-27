becc7434fa703f180ff47975e63eab700e758d9f
singing-cat <519797921@qq.com>
2023-04-27 21:59:34 +0800

    Finished PartA

--- steganography.c
+++ steganography.c
@@ -21,13 +21,33 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+	Color *p = (Color*)malloc(sizeof(Color));
+	uint8_t B = image->image[row*image->cols + col]->B;
+	if(B % 2 == 0) {
+		p->R = 0;
+		p->G = 0;
+		p->B = 0;
+	}
+	else {
+		p->R = 255;
+		p->G = 255;
+		p->B = 255;
+	}
+	return p;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+	Image *p = (Image*)malloc(sizeof(Image));
+	p->cols = image->cols; p->rows = image->rows;
+	p->image = (Color**)malloc(sizeof(Color*)*image->cols*image->rows);
+	for(int i = 0; i < image->rows; i++) {
+		for(int j = 0; j < image->cols; j++) {
+			p->image[i*image->cols+j] = evaluateOnePixel(image, i, j);
+		}
+	}
+	return p;
 }
 
 /*
@@ -45,5 +65,10 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	Image *p = readData(argv[1]);
+	Image *q = steganography(p);
+	writeData(q);
+	freeImage(p);
+	freeImage(q);
+	return 0;
 }
